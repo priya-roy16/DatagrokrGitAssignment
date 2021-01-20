9cfea5303e8044f93f4208f0b0093d3018b462c8
Priya Roy <17bce103@nirmauni.ac.in>
2021-01-20 12:30:45 +0530

    Main Commit

--- my-awesome-script.py
+++ my-awesome-script.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 # This is an awesome Python script to my-awesome-script.py
 
-print(Hajimemashite sekai!)
-=======
-print(Hello! World, we are one!)
->>>>>>> feature/part-1-finale
+print(Commit One)
+print(Commit Two)
+
